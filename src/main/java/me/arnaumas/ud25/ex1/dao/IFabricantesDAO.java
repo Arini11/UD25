5d709625cb5cc3ecb242cf28ff7045f0a3fd49cf
Arini11 <elarnaumc@gmail.com>
2022-08-17 16:31:45 +0200

    ex2

--- src/main/java/me/arnaumas/ud25/ex1/dao/IFabricantesDAO.java
+++
@@ -1,6 +0,0 @@
-package me.arnaumas.ud25.ex1.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import me.arnaumas.ud25.ex1.dto.Fabricantes;
-
-public interface IFabricantesDAO extends JpaRepository<Fabricantes, Long>  {}
